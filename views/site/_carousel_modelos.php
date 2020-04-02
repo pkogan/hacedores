17fe870d30832927de0a17d42bccf3903e20f5f5
cnngimenez <christian.gimenez@fi.uncoma.edu.ar>
2020-04-02 17:28:56 -0300

    Carrousel en el index principal con fotos.

--- views/site/_carousel_modelos.php
+++ views/site/_carousel_modelos.php
@@ -3,18 +3,21 @@ use yii\helpers\Html;
 use app\models\Modelo;
 use yii\bootstrap\Carousel;
 
-$modelos = Modelo::find()->all();
-
 $items = [];
-foreach ($modelos as $modelo){
+$images = [
+    '@web/img/index-carrousel/doctor-1.jpeg',
+    '@web/img/index-carrousel/enfermera-1.jpeg',
+    '@web/img/index-carrousel/impresora-1.jpeg',
+    '@web/img/index-carrousel/mascaras-1.jpeg',
+    '@web/img/index-carrousel/mascaras-2.jpeg',
+    '@web/img/index-carrousel/mascaras-3.jpeg',
+    '@web/img/index-carrousel/mascaras-4.jpeg',
+];
+foreach ($images as $image){
     $items[] = [
-        'content' => Html::img($modelo->imagen,
-                             ['style' => 'height: 200px',
+        'content' => Html::img($image,
+                             ['style' => 'height: 400px',
                               'class' => 'center-block']),
-        'caption' => $modelo->nombre .
-                  ' (' .
-                  Html::a('Ver', ['modelo/view', 'id' => $modelo->idModelo]) .
-                  ')',
     ];
 }
 
