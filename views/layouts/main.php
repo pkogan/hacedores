578f18172462164e691339e4a042264ded536934
cnngimenez <christian.gimenez@fi.uncoma.edu.ar>
2020-04-12 00:35:59 -0300

    Link de contacto en navbar

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -76,46 +76,52 @@ AppAsset::register($this);
                                         \app\models\Rol::ROL_GESTOR])
                     ],
                     ['label'=> 'admin',
-                        'visible' => !Yii::$app->user->isGuest
-                                  && Yii::$app->user->identity->idRol == \app\models\Rol::ROL_ADMIN,
-                        'items'=>[
-                    ['label' => 'Institucion', 'url' => ['/institucion'],
                      'visible' => !Yii::$app->user->isGuest
-                            && in_array(Yii::$app->user->identity->idRol,
-                                       [\app\models\Rol::ROL_ADMIN,
-                                        app\models\Rol::ROL_GESTOR])],
-                    ['label' => 'Pedidos ', 'url' => ['/pedido'],
-                     'visible' => !Yii::$app->user->isGuest
-                            && in_array(Yii::$app->user->identity->idRol,
-                                       [\app\models\Rol::ROL_ADMIN,
-                                        app\models\Rol::ROL_GESTOR])],
+                            && Yii::$app->user->identity->idRol == \app\models\Rol::ROL_ADMIN,
+                     'items'=>[
+                         [
+                             'label' => 'Contactos ', 'url' => ['/contacto/index'],
+                             'visible' => !Yii::$app->user->isGuest
+                                    && Yii::$app->user->identity->idRol == \app\models\Rol::ROL_ADMIN
+                         ],
+                         
+                         ['label' => 'Institucion', 'url' => ['/institucion'],
+                          'visible' => !Yii::$app->user->isGuest
+                                 && in_array(Yii::$app->user->identity->idRol,
+                                            [\app\models\Rol::ROL_ADMIN,
+                                             app\models\Rol::ROL_GESTOR])],
+                         ['label' => 'Pedidos ', 'url' => ['/pedido'],
+                          'visible' => !Yii::$app->user->isGuest
+                                 && in_array(Yii::$app->user->identity->idRol,
+                                            [\app\models\Rol::ROL_ADMIN,
+                                             app\models\Rol::ROL_GESTOR])],
 
-                   /* [ 'label' => 'Usuario',
-                      'items' => [*/
-                          
-                          ['label' => 'Usuarios', 'url' => ['/usuario'],
-                           'visible' => !Yii::$app->user->isGuest
-                                  && Yii::$app->user->identity->idRol == \app\models\Rol::ROL_ADMIN
-                          ],
-                        ]],
+                         /* [ 'label' => 'Usuario',
+                            'items' => [*/
+                         
+                         ['label' => 'Usuarios', 'url' => ['/usuario'],
+                          'visible' => !Yii::$app->user->isGuest
+                                 && Yii::$app->user->identity->idRol == \app\models\Rol::ROL_ADMIN
+                         ],
+                    ]],
                     ['label' => 'Contacto', 'url' => ['/contacto/create'],
                      'visible' => Yii::$app->user->isGuest],
-                        
-                          ['label' => 'Acerca de', 'url' => ['/site/about']],
-                          Yii::$app->user->isGuest ? (
-                              ['label' => 'Login', 'url' => ['/site/login']]
-                          ) : (
-                              '<li>'
-                            . Html::beginForm(['/site/logout'], 'post')
-                            . Html::submitButton(
-                                'Logout (' . Yii::$app->user->identity->username . ')', ['class' => 'btn btn-link logout']
-                            )
-                            . Html::endForm()
-                            . '</li>'
-                          )
-                      ],
-                 /*   ],
-                ],*/
+                    
+                    ['label' => 'Acerca de', 'url' => ['/site/about']],
+                    Yii::$app->user->isGuest ? (
+                        ['label' => 'Login', 'url' => ['/site/login']]
+                    ) : (
+                        '<li>'
+                      . Html::beginForm(['/site/logout'], 'post')
+                      . Html::submitButton(
+                          'Logout (' . Yii::$app->user->identity->username . ')', ['class' => 'btn btn-link logout']
+                      )
+                      . Html::endForm()
+                      . '</li>'
+                    )
+                ],
+                /*   ],
+                   ],*/
             ]);
             NavBar::end();
             ?>
